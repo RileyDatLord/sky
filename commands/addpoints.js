74c683c63f490fc2cc39452eb398f64479b535aa
RileyDatLord <rileydatlord@gmail.com>
2018-03-08 00:18:48 -0800

    Update addpoints.js

--- commands/addpoints.js
+++ commands/addpoints.js
@@ -2,20 +2,16 @@ module.exports = {
     run: async (client, msg, args) => {
         const r = client.db;
         if (!args[0]) {
-            var points = await r.table('globalPoints').get(msg.mentions.users[0].id).run();
-            if (!points) {
-                msg.channel.send(`\\❌ **${msg.mentions.users[0].tag}** does not have a profile. Start chatting to automatically generate one. ${msg.guild.id == '110373943822540800' ? '(Discord Bots is an ignored guild!)' : ''}`);
-            }
-            msg.channel.send(`\\➡ **${msg.mentions.users[0].tag}** has **${points.points}** points.\n\\ℹ You added ${args[1]} to thier points!.`);
+            msg.channel.send('\\❌ No user specified.')
         } else {
             const target = msg.mentions.users.first() || client.users.get(args[0]);
-            if (!target) return msg.channel.send('\\❌ Invalid users.');
+            if (!target) return msg.channel.send('\\❌ Invalid user.');
             if (target.bot) return msg.channel.send('\\❌ Bots do not have Sky Points\\™.');
             var points = await r.table('globalPoints').get(target.id).run();
             if (!points) {
                 msg.channel.send(`\\❌ **${target.tag}** does not have a profile. Get them to start chatting to automatically generate one. ${msg.guild.id == '110373943822540800' ? '(Discord Bots is an ignored guild!)' : ''}`);
             }
-            msg.channel.send(`\\➡ **${target.tag}** has **${points.points}** points.\n\\ℹ You can use \`${client.config.prefix}help points\` for more info on points.`);
+            
         }   
     },
     meta: {
